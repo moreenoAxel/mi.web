2bb6b89aada2ff3ec9f9e862af68788784c39288
moreenoAxel <166864427+moreenoAxel@users.noreply.github.com>
2025-01-28 20:20:03 -0300

    cambio de formato de imagen

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ menuBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden'); 
     mobileMenu.classList.toggle('flex');  
 });
+
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const inputDir = './images';
+const outputDir = '/optimized-images';
+
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+  
+  // Leer todas las imágenes del directorio
+  fs.readdirSync(inputDir).forEach(file => {
+    const inputFilePath = path.join(inputDir, file);
+    const outputFilePath = path.join(outputDir, file);
+  
+    // Optimizar la imagen
+    sharp(inputFilePath)
+      .resize({ width: 800 }) // Cambia el ancho según lo que necesites
+      .toFormat('webp') // Convierte a formato WebP para mejor compresión
+      .webp({ quality: 80 }) // Calidad de la compresión (ajusta según lo necesites)
+      .toFile(outputFilePath)
+      .then(() => {
+        console.log(`Optimizado: ${file}`);
+      })
+      .catch(err => {
+        console.error(`Error optimizando ${file}:`, err);
+      });
+  });
